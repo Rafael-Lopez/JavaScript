f4b338e9239a197feb441f0b19820ad7c5c0acdb
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-09-30 11:58:19 -0400

    Variable mutation and type coercion

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,3 +1,4 @@
+/*
 //String
 var firstName = "John";
 console.log(firstName);
@@ -15,3 +16,29 @@ var job;
 console.log(job);
 job = "Teacher";
 console.log(job);
+*/
+
+/*
+* Variable mutation and type coercion
+*/
+
+//Type coercion
+var firstName = "John";
+var age = 28;
+
+console.log(firstName + " " + age);
+
+var job, isMarried;
+job = "Teacher";
+isMarried = false;
+
+console.log(firstName + " is a " + age + " years old " + job + ". Is he married? " + isMarried);
+
+//Variable mutation
+age = "twenty eight";
+job = "driver";
+
+alert(firstName + " is a " + age + " years old " + job + ". Is he married? " + isMarried);
+
+var lastName = prompt("What is hist last name?");
+console.log(firstName + " " + lastName);
