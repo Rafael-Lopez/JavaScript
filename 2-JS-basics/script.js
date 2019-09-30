3a7572e82a6fd9bd2eb79113aedb8a4fe0bf5316
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-09-30 11:47:18 -0400

    JS variables and data types

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,1 +1,17 @@
-console.log('Hello World!!!');
+//String
+var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+//Number
+var age = 28;
+
+//Boolean
+var fullAge = true;
+console.log(fullAge);
+
+//Undefined
+var job;
+console.log(job);
+job = "Teacher";
+console.log(job);
