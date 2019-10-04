8ca94884a2353a043c06575c8dafa1a214a70390
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-04 14:47:51 -0400

    Callback functions: passing functions as arguments

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -58,3 +58,30 @@ var jane = Object.create(personProto,
 
 //The difference between Object.create and Function constructor is that Object.create builds an object that inherits directly from the one that we passed into the first argument
 //While with the function constructor, the newly created object inherits from the constructor's prototype property.
+
+// **** Passing functions as arguments ****
+
+var years = [1969, 2016, 1989, 1958];
+
+function arrayCalc(years, fn) {
+    var array = [];
+    for (var i = 0; i < years.length; i++) {
+        array.push( fn(years[i]) );
+    }
+    
+    return array;
+}
+
+function calculateAge(element) {
+    return 2016 - element;
+}
+
+function isFullAge(element) {
+    return element >= 18;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(years, isFullAge);
+
+console.log(ages);
+console.log(fullAges);
