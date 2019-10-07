830521bac31cf0af20a591b85365eae3136d822d
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-07 14:42:53 -0400

    Adding first event listeners

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -1,11 +1,27 @@
+//BUDGET CONTROLLER
 var budgetController = (function() {
     
 })();
 
+//UI CONTROLLER
 var UIController = (function() {
     
 })();
 
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+    
+    var ctrlAddItem = function() {
+        console.log("Test");
+    };
+    
+    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    
+    //Happens on the global web page
+    document.addEventListener("keypress", function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
     
 })(budgetController, UIController);
