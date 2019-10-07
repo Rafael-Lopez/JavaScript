8b42625e94411d06c7b97fd7afd611cffb6def75
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-07 15:01:18 -0400

    Reading input data

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -3,19 +3,49 @@ var budgetController = (function() {
     
 })();
 
+
+
 //UI CONTROLLER
 var UIController = (function() {
     
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addButton: ".add__btn"
+    };
+    
+    return {
+        getInput: function() {
+            
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //Will be either Inc or Exp (it's a Select component. Look at the HTML)
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        }, 
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+    
 })();
+
+
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
     
+    var DOMstrings = UICtrl.getDOMstrings();
+    
     var ctrlAddItem = function() {
-        console.log("Test");
+        
+        var input = UICtrl.getInput();
+        console.log(input);
+        
     };
     
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    document.querySelector(DOMstrings.addButton).addEventListener("click", ctrlAddItem);
     
     //Happens on the global web page
     document.addEventListener("keypress", function(event) {
