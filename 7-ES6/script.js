af2dfdbf0775dd7f1e3e5f132b1b18af8f1a7eae
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-08 13:13:19 -0400

    ES6: Variable declarations (let and const)

---
+++ 7-ES6/script.js
@@ -0,0 +1,11 @@
+// ******  ES6  ******
+
+// In ES6, use let and const instead of var
+let age = 23;
+const = yearOfBirth = 1996;
+
+// let and const have block scope (code between { and })
+// var has function scope
+
+// In ES5, you can use a variable without declaring it and it will have a value of Undefined
+// In ES6, you need to declare the variable first. Otherwise you get an error.
