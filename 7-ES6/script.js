48b311e0de588e2c54cd27a2580da35a7357fc8b
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-08 15:21:51 -0400

    ES6: Arrow Functions 2

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -79,3 +79,63 @@ ages6 = years.map( (current, index) => {
     return `Age element ${index + 1}: ${age}.`;
 });
 console.log(ages6);
+
+/*
+    ***** Arrow Functions 2 *****
+*/
+
+// ES5
+// Only in method calls, the 'this' keyword points to that object. But in a regular function call, the 'this' keyword points to the global object.
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        document.querySelector('.green').addEventListener('click', function() {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+};
+box5.clickMe();
+// To solve the problem. You can add "var self = this;" before querySelector, and replace the 'this' keyword with self instead.
+
+// ES6
+// The arrow functions share the lexical 'this' kewyword of their sorrundings
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+};
+box6.clickMe();
+
+
+
+function Person(name) {
+    this.name = name;
+}
+
+// ES5
+Person.prototype.myFriends5 = function() {
+    var arr = friends.map( function(el) {
+        return this.name + ' is friends with ' + el;
+    }.bind(this) );
+    
+    console.log(arr);
+};
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+
+// ES6
+Person.prototype.myFriends6 = function() {
+    let arr = friends.map( el => `${this.name} is friends with ${el}` );
+    console.log(arr);
+};
+
+let friends6 = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends6(friends6);
