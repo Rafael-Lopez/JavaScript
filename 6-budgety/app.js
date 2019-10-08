88e3c61431534fd91580fb6c6e1fde6e0785692e
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-08 12:15:34 -0400

    Adding current date to project

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -155,7 +155,8 @@ var UIController = (function() {
         expensesLabel: ".budget__expenses--value",
         percentageLabel: ".budget__expenses--percentage",
         container: ".container",
-        expensesPercentageLabel: ".item__percentage"
+        expensesPercentageLabel: ".item__percentage",
+        dateLabel: ".budget__title--month"
     };
     
     var formatNumber = function(num, type) {
@@ -277,6 +278,18 @@ var UIController = (function() {
                     current.textContent = "---";
                 }
             });
+        },
+        
+        displayMonth: function() {
+            
+            var now, year, month, months;
+            
+            now = new Date();
+            year = now.getFullYear();
+            months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            month = now.getMonth();
+            
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + " " + year;
         }
     };
     
@@ -383,6 +396,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log("App started");
+            UICtrl.displayMonth();
             UICtrl.displayBudget( {
                 budget: 0,
                 totalInc: 0,
