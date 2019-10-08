5e208e5e5a9ebf63c61441a7220950e262ae7b73
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-08 13:17:39 -0400

    ES6: Blocks and IIFEs

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -9,3 +9,20 @@ const = yearOfBirth = 1996;
 
 // In ES5, you can use a variable without declaring it and it will have a value of Undefined
 // In ES6, you need to declare the variable first. Otherwise you get an error.
+
+/*
+    ***** Blocks and IIFEs *****
+*/
+
+// IIFE in ES5
+( function() {
+    var a = 1;
+    var b = 2; 
+})();
+
+// In ES6, for data privacy, you can do this instead
+{
+    const a = 1;
+    let b = 2;
+}
+
