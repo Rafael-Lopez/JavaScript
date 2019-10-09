78033bcd7e37d50ebb1b33c26a6dc36cd9681691
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-09 09:51:40 -0400

    ES6: Maps

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -337,3 +337,42 @@ function SmithPerson6(firstName, yearOfBirth, lastName = 'Smith', nationality = 
 const johnA = new SmithPerson6('John', 1990);
 
 console.log(johnA);
+
+
+
+
+
+/*
+    ***** Maps *****
+*/
+
+const question = new Map();
+question.set('question', 'What is the official names of the latest major JS version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer!');
+question.set(false, 'Wrong answer!');
+
+console.log(question.get('question'));
+console.log(question.size);
+
+/*if(question.has(4)) {
+    question.delete(4)
+}
+
+question.clear();*/
+
+question.forEach( (value, key) => console.log(`This is the key: ${key}, and this is the value: ${value}`));
+
+for(let [key, value] of question.entries()) {
+    if(typeof(key) === 'number') {
+        console.log(`Answer ${key}: ${value}`);
+    }   
+}
+
+const ans = parseInt(prompt('Write the correct answer'));
+
+console.log( question.get(ans === question.get('correct')) ); 
