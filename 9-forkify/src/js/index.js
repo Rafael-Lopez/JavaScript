159cb65baf684c6b16d6e255345e4c23857dd8e0
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-10 11:52:28 -0400

    Implementing Search Results Pagination

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -38,3 +38,14 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
+
+elements.searchResPages.addEventListener('click', e => {
+    // Because you can click on the text or arrow in the button, wich are different elements from the button
+    const btn = e.target.closest('.btn-inline');
+    
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
