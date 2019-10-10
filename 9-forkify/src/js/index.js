5bfbcfe9172db83e79890db51e3b88d04cb8b74c
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-10 09:33:35 -0400

    Adding Axios for AJAX calls

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -14,3 +14,15 @@ console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3,5)}. ${str
 //Third way to do multiple imports
 //import * as searchView from './views/searchView';
 //console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}. ${str}`);
+
+
+// API key: 65778a9329a40afec80fb8874bcd787f
+// https://www.food2fork.com/api/search
+
+import axios from 'axios';
+
+async function getResults(query) {
+    
+}
+
+getResults();
