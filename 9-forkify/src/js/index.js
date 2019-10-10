a775bf36b31225e36b1d380590af3031e91dd828
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-10 10:03:27 -0400

    Building the Search model

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,21 +1,8 @@
 // Global app controller
 
-// API key: 65778a9329a40afec80fb8874bcd787f
-// https://www.food2fork.com/api/search
+import Search from './models/Search';
 
-import axios from 'axios';
+const search = new Search('pizza');
+console.log(search.getResults());
 
-async function getResults(query) {
-    
-    const key = '65778a9329a40afec80fb8874bcd787f';
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    try {
-        const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = result.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-}
 
-getResults('pizza');
--- 9-forkify/src/js/models/Search.js
+++ 9-forkify/src/js/models/Search.js
@@ -1,2 +1,19 @@
-// To export just one thing from a module, do it this way
-export default 'I am an export String';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+    
+    async getResults() {   
+        const key = '65778a9329a40afec80fb8874bcd787f';
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        try {
+            const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = result.data.recipes;
+            //console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
